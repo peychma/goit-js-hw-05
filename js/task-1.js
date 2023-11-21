6c35e72816adb150205738f7ef7a801e5eafd18e
peychma <142826840+peychma@users.noreply.github.com>
2023-11-21 10:54:33 +0200

    Add files via upload

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+function isEnoughCapacity(products, containerSize) {
+    const quantities = Object.values(products);
+    const totalQuantity = quantities.reduce((sum, quantity) => sum + quantity, 0);
+    return totalQuantity <= containerSize;
+}
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7));
